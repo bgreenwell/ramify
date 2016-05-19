8b2bae92ce905533c48a99c0c2a1608565dd14d6
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-05-19 10:12:14 -0400

    fixed test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,7 +3,7 @@ context("Utility functions")
 test_that("utility functions work correctly", {
 
   expect_identical(add_dots(1:10, pos = 3), c("1", "2", "...", "10"))
-  expect_identical(add_dots(1:10, pos = 10), c("1", "2", "...", "10"))
+  expect_identical(add_dots(1:10, pos = 10), 1:10)
 
   m1 <- matrix(1:4, nrow = 2)
   m2 <- matrix(c(1.0, 2.0, 3.0, 4.0), nrow = 2)
