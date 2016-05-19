855eadfdbf2de934257fbe9889cdadf91fae6548
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-05-19 11:24:13 -0400

    more tests

--- tests/testthat/test-matrix_rank.R
+++ tests/testthat/test-matrix_rank.R
@@ -8,5 +8,6 @@ test_that("matrix_rank computes the correct rank of a matrix", {
   expect_identical(matrix_rank(x1), 1L)
   expect_identical(matrix_rank(cbind(x1, x2)), 2L)
   expect_identical(matrix_rank(cbind(x1, 2*x1)), 1L)
-  
+  expect_identical(matrix_rank(as.data.frame(cbind(x1, x2))), 2L)
+
 })
